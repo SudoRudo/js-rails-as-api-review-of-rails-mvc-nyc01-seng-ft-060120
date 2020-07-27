685f3f33a8fb468a2d3d06a99669494f23fc50cf
Rodolfo  Guerrero-Martinez II <r.guerreronyc@gmail.com>
2020-07-27 22:04:15 +0000

    Automatically backed up by Learn

--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class CreateBirds < ActiveRecord::Migration[5.2]
     def change
       create_table :birds do |t|
@@ -8,3 +9,16 @@ class CreateBirds < ActiveRecord::Migration[5.2]
       end
     end
   end
+=======
+
+class CreateBirds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :birds do |t|
+      t.string :name
+      t.string :species
+ 
+      t.timestamps
+    end
+  end
+end
+>>>>>>> 5022e04989c5783c7b9d133fb1664f2c32998ebc
